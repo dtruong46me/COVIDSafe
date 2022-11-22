66576777f870ae4a352a9fafa53328648b0f45f2
dtruong46me <dnhtruong46.cs.pyth@gmail.com>
2022-11-22 08:50:55 +0700

    Convert to List

--- creat_virus.py
+++ creat_virus.py
@@ -1,11 +1,15 @@
 import numpy as np
 n = 16
-
-lst0 = [0 for i in range(16*16-40)]
-lst1 = [1 for i in range(40)]
+virus = 40
+lst0 = [0 for i in range(n*n-virus)]
+lst1 = [1 for i in range(virus)]
 lst = lst0+lst1
 arr = np.array(lst)
 # print(arr)
 arr2 = np.random.permutation(arr)
 # print(arr)
-print(arr2.reshape(16,16))
+arr3 = arr2.reshape(16,16)
+# print(arr2)
+# print(arr3)
+my_lst = arr3.tolist()
+print(my_lst)
