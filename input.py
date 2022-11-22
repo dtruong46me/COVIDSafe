ddea9df4ea117e4b4456597e048897e9872e7d00
dtruong46me <dnhtruong46.cs.pyth@gmail.com>
2022-11-22 14:54:05 +0700

    Update Input

--- input.py
+++ input.py
@@ -3,6 +3,11 @@ def instruction():
     print('1. Enter row and column to open a cell. Example: \'4 6\'')
     print('2. Enter row and column and letter \'F\' to open a cell. Example: \'4 6 F\'')
 n = 16
+
+import os
+def clear():
+    os.system('clear')
+
 print(instruction())
 while True:
     myInput = input().split()
@@ -12,23 +17,27 @@ while True:
         try:
             val = list(map(int,myInput))
         except ValueError:
+            clear()
             print('Wrong input!')
             print(instruction())
             continue
     
     elif len(myInput) == 3:
         if myInput[2] != 'F' and myInput[2] != 'f':
+            clear()
             print('Wrong input!')
             print(instruction())
         
         try:
             val = list(map(int, myInput[:2]))
         except ValueError:
+            clear()
             print('Wrong input!')
             print(instruction())
             continue
 
         if val[0]<=0 or val[1]<=0 or val[1]>=n or val[1]>=n:
+            clear()
             print('Wrong input!')
             print(instruction())
             continue
